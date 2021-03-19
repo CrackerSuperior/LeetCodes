5a54b2aaf59413ab2446dc7f1b949fc939e7896f
Cracker <2278801557@qq.com>
2021-03-19 17:25:50 +0800

    new project

---
+++ src/com/Cracker/Tags/Stack/ValidParenthesis.java
@@ -0,0 +1,16 @@
+package com.Cracker.Tags.Stack;
+
+/**
+ * @ClassName ValidParenthesis
+ * @Description TODO
+ * @Author Cracker
+ * @Date 2021/3/19 17:04
+ */
+public class ValidParenthesis {
+
+    public boolean isValid(String s) {
+
+        return false;
+    }
+
+}
