b1e8bb7e3b7b7f15acc57e482e9ce893df582ddb
Cracker <2278801557@qq.com>
2022-03-11 10:05:28 +0800

    Leetcode number 13: Roman to Integer.

--- src/main/java/com/cracker/leetcodes/map/RomanToInteger.java
+++ src/main/java/com/cracker/leetcodes/map/RomanToInteger.java
@@ -42,4 +42,30 @@ public class RomanToInteger {
         List<Integer> collect = Arrays.stream(split).map(Integer::parseInt).collect(Collectors.toList());
         return collect.stream().mapToInt(each -> each).sum();
     }
+
+    public int romanToIntPlus(final String s) {
+        int result = 0;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length - 1; i++) {
+            if (getValue(chars[i]) < getValue(chars[i + 1])) {
+                result -= getValue(chars[i]);
+            } else {
+                result += getValue(chars[i]);
+            }
+        }
+        return result + getValue(chars[chars.length - 1]);
+    }
+
+    private int getValue(final Character character) {
+        switch (character) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
 }
