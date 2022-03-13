bd70537aef70e2da3a6922ab56fee2feeaee8961
Cracker <2278801557@qq.com>
2022-03-13 23:05:16 +0800

    Leetcode number 26: Remove Duplicates from Sorted Array.

--- src/main/java/com/cracker/leetcodes/array/RemoveDuplicatesFormSortedArray.java
+++ src/main/java/com/cracker/leetcodes/array/RemoveDuplicatesFormSortedArray.java
@@ -9,9 +9,7 @@ public class RemoveDuplicatesFormSortedArray {
 
     public int removeDuplicates(final int[] nums) {
         int[] results = Arrays.stream(nums).distinct().toArray();
-        for (int i = 0; i < results.length; i++) {
-            nums[i] = results[i];
-        }
+        System.arraycopy(results, 0, nums, 0, results.length);
         return results.length;
     }
 
