a586b720839f95e535584323eb62f3b5994fe589
Cracker <2278801557@qq.com>
2022-03-23 21:25:38 +0800

    Leetcode number 226: Invert Binary Tree.

---
+++ src/main/java/com/cracker/leetcodes/tree/InvertBinaryTree.java
@@ -0,0 +1,51 @@
+package com.cracker.leetcodes.tree;
+
+/**
+ * This is Leetcode number 226: Invert Binary Tree.
+ */
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(final TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    public TreeNode invertTree2(final TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+
+    public static class TreeNode {
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
