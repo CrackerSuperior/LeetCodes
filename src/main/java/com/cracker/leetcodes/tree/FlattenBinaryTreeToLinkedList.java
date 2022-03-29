4b7badc9262725d92190e96a5fcb47c5baa339bb
Cracker <2278801557@qq.com>
2022-03-29 23:16:38 +0800

    Leetcode number 114: Flatten Binary Tree to Linked List.

---
+++ src/main/java/com/cracker/leetcodes/tree/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,46 @@
+package com.cracker.leetcodes.tree;
+
+/**
+ * This is Leetcode number 114: Flatten Binary Tree to Linked List.
+ */
+public class FlattenBinaryTreeToLinkedList {
+
+    public void flatten(final TreeNode root) {
+        if (null == root) {
+            return;
+        }
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = null;
+        root.right = left;
+        TreeNode p = root;
+        while (null != p.right) {
+            p = p.right;
+        }
+        p.right = right;
+    }
+
+    public class TreeNode {
+
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
