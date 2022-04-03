61b13b1eef94408b69f1619e80a62c4f916dcdc0
Cracker <2278801557@qq.com>
2022-04-03 22:26:24 +0800

    Leetcode number 297: Serialize and Deserialize Binary Tree.

---
+++ src/main/java/com/cracker/leetcodes/tree/SerializeAndDeserializeBinaryTree.java
@@ -0,0 +1,68 @@
+package com.cracker.leetcodes.tree;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * his is Leetcode number 297: Serialize and Deserialize Binary Tree.
+ */
+public class SerializeAndDeserializeBinaryTree {
+
+    public String serialize(final TreeNode root) {
+        LinkedList<String> results = new LinkedList<>();
+        build(results, root);
+        return String.join(",", results);
+    }
+
+    public TreeNode deserialize(final String data) {
+        String[] results = data.split(",");
+        LinkedList<String> strings = new LinkedList<>(Arrays.asList(results));
+        return build(strings);
+    }
+
+    private void build(final List<String> results, final TreeNode root) {
+        if (null == root) {
+            results.add("null");
+            return;
+        }
+        results.add(String.valueOf(root.val));
+        build(results, root.left);
+        build(results, root.right);
+    }
+
+    private TreeNode build(final LinkedList<String> results) {
+        if (results.isEmpty()) {
+            return null;
+        }
+        String first = results.removeFirst();
+        if ("null".equals(first)) {
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.parseInt(first));
+        root.left = build(results);
+        root.right = build(results);
+        return root;
+    }
+
+    public static class TreeNode {
+
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
