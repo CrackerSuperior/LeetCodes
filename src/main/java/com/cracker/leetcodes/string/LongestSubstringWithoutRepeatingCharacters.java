f9129179ec41558da393378a08d5a13f8fa3d010
Cracker <2278801557@qq.com>
2022-04-28 16:17:23 +0800

    Leetcode number 3: Longest Substring Without Repeating Characters.

---
+++ src/main/java/com/cracker/leetcodes/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,31 @@
+package com.cracker.leetcodes.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This is Leetcode number 3: Longest Substring Without Repeating Characters.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> window = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        int res = 0;
+        char[] chars = s.toCharArray();
+        while (right < s.length()) {
+            char aChar = chars[right++];
+            window.put(aChar, window.getOrDefault(aChar, 0) + 1);
+            while (window.get(aChar) > 1) {
+                char c = chars[left];
+                if (window.containsKey(c)) {
+                    window.put(c, window.getOrDefault(c, 0) - 1);
+                }
+                left++;
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
+}
