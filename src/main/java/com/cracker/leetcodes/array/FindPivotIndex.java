c59dea67d4438d9486783fe256f4c68962a15e28
Cracker <2278801557@qq.com>
2022-05-10 16:50:02 +0800

    Leetcode number 724: Find Pivot Index.

---
+++ src/main/java/com/cracker/leetcodes/array/FindPivotIndex.java
@@ -0,0 +1,21 @@
+package com.cracker.leetcodes.array;
+
+/**
+ * This is Leetcode number 724: Find Pivot Index.
+ */
+public class FindPivotIndex {
+
+    public int pivotIndex(int[] nums) {
+        int[] prefixSum = new int[nums.length + 1];
+        prefixSum[0] = 0;
+        for (int i = 1; i < prefixSum.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] - nums[i - 1];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (prefixSum[i] == prefixSum[nums.length] - prefixSum[i + 1]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
