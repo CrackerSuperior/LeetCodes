e7908adceca68ae0c000a89b6b8b63a3e5df09a6
Cracker <2278801557@qq.com>
2022-05-12 17:09:34 +0800

    Leetcode number 704: Binary Search.

---
+++ src/main/java/com/cracker/leetcodes/array/ContinuousSubarraySum.java
@@ -0,0 +1,29 @@
+package com.cracker.leetcodes.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This is Leetcode number 704: Binary Search.
+ */
+public class ContinuousSubarraySum {
+
+    public boolean checkSubarraySum(final int[] nums, final int k) {
+        int[] prefixSum = new int[nums.length + 1];
+        prefixSum[0] = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 0);
+        for (int i = 1; i < prefixSum.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+            int key = prefixSum[i] % k;
+            if (!map.containsKey(key)) {
+                map.put(key, i);
+            } else {
+                if (i - map.get(key) >= 2) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
