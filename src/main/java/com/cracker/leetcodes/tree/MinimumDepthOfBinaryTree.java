5a6a4d1b3783803634c985c15fe9cd8a93915941
Cracker <2278801557@qq.com>
2022-06-11 22:34:20 +0800

    Leetcode number 111: Minimum Depth of Binary Tree.

---
+++ src/main/java/com/cracker/leetcodes/tree/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,51 @@
+package com.cracker.leetcodes.tree;
+
+/**
+ * This is Leetcode number 111: Minimum Depth of Binary Tree.
+ */
+public class MinimumDepthOfBinaryTree {
+
+    private int minDepth;
+
+    public int minDepth(final TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        minDepth = Integer.MAX_VALUE;
+        dp(root, 1);
+        return minDepth;
+    }
+
+    private void dp(final TreeNode root, final int depth) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            minDepth = Math.min(minDepth, depth);
+            return;
+        }
+        dp(root.left, depth + 1);
+        dp(root.right, depth + 1);
+    }
+
+    public static class TreeNode {
+
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
