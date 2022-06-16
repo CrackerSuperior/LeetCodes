18834c9146ed60dbc62578d638e95d1230a611c6
Cracker <2278801557@qq.com>
2022-06-16 14:24:08 +0800

    Leetcode number 669: Trim a Binary Search Tree.

---
+++ src/main/java/com/cracker/leetcodes/tree/TrimABinarySearchTree.java
@@ -0,0 +1,43 @@
+package com.cracker.leetcodes.tree;
+
+/**
+ * This is Leetcode number 669: Trim a Binary Search Tree.
+ */
+public class TrimABinarySearchTree {
+
+    public TreeNode trimBST(final TreeNode root, final int low, final int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+
+    public static class TreeNode {
+
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
