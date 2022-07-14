4737ab9068167b6a44892822e77da1c9f6e672ed
Cracker <2278801557@qq.com>
2022-07-14 15:07:01 +0800

    Leetcode number 701: Insert into a Binary Search Tree.

---
+++ src/main/java/com/cracker/leetcodes/tree/InsertIntoABinarySearchTree.java
@@ -0,0 +1,40 @@
+package com.cracker.leetcodes.tree;
+
+/**
+ * This is Leetcode number 701: Insert into a Binary Search Tree.
+ */
+public class InsertIntoABinarySearchTree {
+
+    public TreeNode insertIntoBST(final TreeNode root, final int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val > root.val) {
+            root.right = insertIntoBST(root.right, val);
+        } else if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+
+    public static class TreeNode {
+
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
