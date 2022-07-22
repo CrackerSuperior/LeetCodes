881db361eb9600bfae5f5957bfa823d4c37deb49
Cracker <2278801557@qq.com>
2022-07-22 15:02:59 +0800

    Leetcode number 236: Lowest Common Ancestor of a Binary Tree.

---
+++ src/main/java/com/cracker/leetcodes/tree/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,43 @@
+package com.cracker.leetcodes.tree;
+
+/**
+ * This is Leetcode number 236: Lowest Common Ancestor of a Binary Tree.
+ */
+public class LowestCommonAncestorOfABinaryTree {
+
+    public TreeNode lowestCommonAncestor(final TreeNode root, final TreeNode p, final TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left != null ? left : right;
+    }
+
+    public static class TreeNode {
+
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
