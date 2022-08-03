b79b2adae4f3d29a29c425a4547a90829d93bf5d
Cracker <2278801557@qq.com>
2022-08-03 16:00:24 +0800

    Leetcode number 508: Most Frequent Subtree Sum.

---
+++ src/main/java/com/cracker/leetcodes/tree/MostFrequentSubtreeSum.java
@@ -0,0 +1,65 @@
+package com.cracker.leetcodes.tree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This is Leetcode number 508: Most Frequent Subtree Sum.
+ */
+public class MostFrequentSubtreeSum {
+    
+    private final Map<Integer, Integer> result = new HashMap<>();
+
+    public int[] findFrequentTreeSum(final TreeNode root) {
+        dp(root);
+        int max = Integer.MIN_VALUE;
+        for (Integer value : result.values()) {
+            max = max < value ? value : max;
+        }
+        List<Integer> resultList = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
+            if (entry.getValue().equals(max)) {
+                resultList.add(entry.getKey());
+            }
+        }
+        int[] res = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            res[i] = resultList.get(i);
+        }
+        return res;
+    }
+    
+    private int dp(final TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dp(root.left);
+        int right = dp(root.right);
+        int sum = root.val + left + right;
+        result.put(sum, result.getOrDefault(sum, 0) + 1);
+        return sum;
+    }
+
+    public static class TreeNode {
+
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
