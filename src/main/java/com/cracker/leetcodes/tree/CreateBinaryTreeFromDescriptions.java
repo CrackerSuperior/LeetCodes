81070437576c30383ed906a2972962832a584d21
Cracker <2278801557@qq.com>
2022-08-04 17:28:30 +0800

    Leetcode number 2196: Create Binary Tree From Descriptions.

---
+++ src/main/java/com/cracker/leetcodes/tree/CreateBinaryTreeFromDescriptions.java
@@ -0,0 +1,81 @@
+package com.cracker.leetcodes.tree;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This is Leetcode number 2196: Create Binary Tree From Descriptions.
+ */
+public class CreateBinaryTreeFromDescriptions {
+
+    public TreeNode createBinaryTree(final int[][] descriptions) {
+        Map<Integer, TreeNode> nodes = new HashMap<>();
+        Set<Integer> childNodes = new HashSet<>();
+        for (int[] description : descriptions) {
+            int parent = description[0];
+            int child = description[1];
+            int left = description[2];
+            if (!nodes.containsKey(parent)) {
+                nodes.put(parent, new TreeNode(parent));
+            }
+            if (!nodes.containsKey(child)) {
+                nodes.put(child, new TreeNode(child));
+            }
+            if (left == 1) {
+                nodes.get(parent).left = nodes.get(child);
+            } else {
+                nodes.get(parent).right = nodes.get(child);
+            }
+            childNodes.add(child);
+        }
+        for (Integer node : nodes.keySet()) {
+            if (!childNodes.contains(node)) {
+                return nodes.get(node);
+            }
+        }
+        return null;
+    }
+
+
+    public TreeNode createBinaryTreePlus(final int[][] descriptions) {
+        TreeNode[] nodes = new TreeNode[100001];
+        for (int[] node : descriptions) {
+            nodes[node[1]] = new TreeNode(node[1]);
+        }
+        int root = 0;
+        for (int[] node : descriptions) {
+            if (null == nodes[node[0]]) {
+                nodes[node[0]] = new TreeNode(root = node[0]);
+            }
+            if (node[2] == 1) {
+                nodes[node[0]].left = nodes[node[1]];
+            } else {
+                nodes[node[0]].right = nodes[node[1]];
+            }
+        }
+        return nodes[root];
+    }
+
+    public static class TreeNode {
+
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
