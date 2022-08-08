c44e3bac7cd6230b4d8af7401c87fe8e9a65c543
Cracker <2278801557@qq.com>
2022-08-08 15:56:23 +0800

    Leetcode number 814: Binary Tree Pruning.

---
+++ src/main/java/com/cracker/leetcodes/tree/BinaryTreePruning.java
@@ -0,0 +1,40 @@
+package com.cracker.leetcodes.tree;
+
+/**
+ * This is Leetcode number 814: Binary Tree Pruning.
+ */
+public class BinaryTreePruning {
+
+    public TreeNode pruneTree(final TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0) {
+            return null;
+        }
+        return root;
+    }
+
+    public static class TreeNode {
+
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
