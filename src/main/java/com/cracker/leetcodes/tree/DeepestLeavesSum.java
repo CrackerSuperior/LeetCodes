15e67db42c9a9252a175729f35496058b4f01010
Cracker <2278801557@qq.com>
2022-08-16 14:06:53 +0800

    Leetcode number 1302: Deepest Leaves Sum.

---
+++ src/main/java/com/cracker/leetcodes/tree/DeepestLeavesSum.java
@@ -0,0 +1,78 @@
+package com.cracker.leetcodes.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * This is Leetcode number 1302: Deepest Leaves Sum.
+ */
+public class DeepestLeavesSum {
+
+    private int maxDepth;
+
+    private int result;
+
+    public int deepestLeavesSum(final TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> treeNodes = new LinkedList<>();
+        treeNodes.offer(root);
+        int sum = 0;
+        while (!treeNodes.isEmpty()) {
+            sum = 0;
+            int size = treeNodes.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = treeNodes.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    treeNodes.offer(node.left);
+                }
+                if (node.right != null) {
+                    treeNodes.offer(node.right);
+                }
+            }
+        }
+        return sum;
+    }
+
+    public int deepestLeavesSumPlus(final TreeNode root) {
+        dp(root, 0);
+        return result;
+    }
+    
+    private void dp(final TreeNode root, final int depth) {
+        if (root == null) {
+            return;
+        }
+        if (depth == maxDepth) {
+            result += root.val;
+        } else if (depth > maxDepth) {
+            maxDepth = depth;
+            result = root.val;
+        }
+        dp(root.left, depth + 1);
+        dp(root.right, depth + 1);
+    }
+
+    public static class TreeNode {
+
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
