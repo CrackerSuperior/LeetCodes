782131410f919d83f68677967619f1cfdc02de61
Cracker <2278801557@qq.com>
2022-08-28 22:24:17 +0800

    Leetcode Offer 32 - II: Printing the binary tree from top to bottom II.

---
+++ src/main/java/com/cracker/leetcodes/tree/Offer32IIPrintingTheBinaryTreeFromTopToBottomII.java
@@ -0,0 +1,59 @@
+package com.cracker.leetcodes.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * This is Leetcode Offer 32 - II: Printing the binary tree from top to bottom II.
+ *      This question is the same as leetcode's 102 questions.
+ */
+public class Offer32IIPrintingTheBinaryTreeFromTopToBottomII {
+
+    public List<List<Integer>> levelOrder(final TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> tree = new LinkedList<>();
+        tree.offer(root);
+        while (!tree.isEmpty()) {
+            List<Integer> levelNodes = new ArrayList<>();
+            int size = tree.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = tree.poll();
+                levelNodes.add(node.val);
+                if (node.left != null) {
+                    tree.offer(node.left);
+                }
+                if (node.right != null) {
+                    tree.offer(node.right);
+                }
+            }
+            result.add(levelNodes);
+        }
+        return result;
+    }
+
+    public static class TreeNode {
+
+        private int val;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(final int val) {
+            this.val = val;
+        }
+
+        TreeNode(final int val, final TreeNode left, final TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
