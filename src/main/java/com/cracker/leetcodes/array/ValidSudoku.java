fe9d8d72aeaaaa381d8ead1366f3c37d1f6e9a28
Cracker <2278801557@qq.com>
2022-08-30 15:59:49 +0800

    Leetcode number 36: Valid Sudoku.

---
+++ src/main/java/com/cracker/leetcodes/array/ValidSudoku.java
@@ -0,0 +1,28 @@
+package com.cracker.leetcodes.array;
+
+/**
+ * This is Leetcode number 36: Valid Sudoku.
+ */
+public class ValidSudoku {
+
+    public boolean isValidSudoku(final char[][] board) {
+        int[][] rows = new int[9][9];
+        int[][] columns = new int[9][9];
+        int[][][] subBoxes = new int[3][3][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    int index = c - '0' - 1;
+                    rows[i][index]++;
+                    columns[j][index]++;
+                    subBoxes[i / 3][j / 3][index]++;
+                    if (rows[i][index] > 1 || columns[j][index] > 1 || subBoxes[i / 3][j / 3][index] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
