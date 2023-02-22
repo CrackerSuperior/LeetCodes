d423413fbbb4a0055858e7ebb86036531ac5bdfe
Cracker <2278801557@qq.com>
2023-02-22 15:11:07 +0800

    Leetcode number 877: Stone Game.

---
+++ src/main/java/com/cracker/leetcodes/math/StoneGame.java
@@ -0,0 +1,12 @@
+package com.cracker.leetcodes.math;
+
+/**
+ * This is Leetcode number 877: Stone Game.
+ */
+public class StoneGame {
+
+    public boolean stoneGame(final int[] piles) {
+        //The First move must win
+        return true;
+    }
+}
